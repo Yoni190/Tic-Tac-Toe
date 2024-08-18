56be3abc84557b1f691afe8cb68215d2b09f73a5
Yoni190 <yfisuhu@gmail.com>
2024-08-18 12:46:29 +0300

    Create a method to mark a location on the board

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,20 @@
 class Board
+  attr_accessor :board
+
   def initialize 
-    puts "1 | 2 | 3\n" +
+  self.board =  "1 | 2 | 3\n" +
         "---+---+---\n" +
          "4 | 5 | 6\n" +
         "---+---+---\n" +
          "7 | 8 | 9"
+  end
+
+  def display_board
+    puts board
+  end
+
+  def mark_the_board(choice, mark)
+    board.sub!(choice, mark)
+    puts board
   end
 end
--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,17 @@
 require_relative 'board'
+require_relative 'player'
 
 class Game
   @@player_turn = 0
 
   def initialize
     players = [Player.new, Player.new]
-    Board.new
+    board = Board.new
+    board.display_board
+
     puts "#{players[@@player_turn].name}, mark your mark"
+    choice = gets.chomp
+    mark = players[@@player_turn].mark
+    board.mark_the_board(choice, mark)
   end
 end
