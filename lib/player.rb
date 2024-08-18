4e4d22287ef5fdd8c0a4e2353976db67ef2205a5
Yoni190 <yfisuhu@gmail.com>
2024-08-18 12:22:34 +0300

    Make player class use user input when initialized

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,10 @@
 class Player
   attr_accessor :name, :mark
 
-  def initialize(n, m)
-    name = n
-    mark = m
+  def initialize
+    puts "What is your name?: "
+    name = gets.chomp
+    puts "What is your mark?: "
+    mark = gets.chomp
   end
 end
