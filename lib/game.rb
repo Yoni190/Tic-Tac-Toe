46fcce4529f692ccef3c94976f643bc024bbeb9a
Yoni190 <yfisuhu@gmail.com>
2024-08-18 12:49:34 +0300

    Create a method to play the game

--- lib/game.rb
+++ lib/game.rb
@@ -3,15 +3,20 @@ require_relative 'player'
 
 class Game
   @@player_turn = 0
+  @@players = []
 
   def initialize
-    players = [Player.new, Player.new]
+    @@players = [Player.new, Player.new]
     board = Board.new
     board.display_board
 
+    
+  end
+  def play_game
     puts "#{players[@@player_turn].name}, mark your mark"
     choice = gets.chomp
     mark = players[@@player_turn].mark
     board.mark_the_board(choice, mark)
+    @@player_turn = @@player_turn == 0 ? 1 : 0
   end
 end
