62a6c0f912e1b8ed7c5f0c430bd6259a858e3daa
Yoni190 <yfisuhu@gmail.com>
2024-08-18 13:01:23 +0300

    Create a hash to store player choices

--- lib/game.rb
+++ lib/game.rb
@@ -4,19 +4,25 @@ require_relative 'player'
 class Game
   @@player_turn = 0
   @@players = []
+  @@player_choices = {}
+  WIN_PATTERNS = ["123", "456", "789"]
 
   def initialize
     @@players = [Player.new, Player.new]
     board = Board.new
     board.display_board
+    @@player_choices[@@players[0].name] = "0"
+    @@player_choices[@@players[1].name] = "0"
 
-    
   end
+
   def play_game
-    puts "#{players[@@player_turn].name}, mark your mark"
+    player_name = @@players[@@player_turn].name
+    puts "#{player_name}, mark your mark"
     choice = gets.chomp
     mark = players[@@player_turn].mark
     board.mark_the_board(choice, mark)
+    @@player_choices[player_name] = 
     @@player_turn = @@player_turn == 0 ? 1 : 0
   end
 end
