b1393666b88598c3870944047ea82f12168261cd
Yoni190 <yfisuhu@gmail.com>
2024-08-18 13:34:57 +0300

    Make players take turns

--- lib/game.rb
+++ lib/game.rb
@@ -9,20 +9,25 @@ class Game
 
   def initialize
     @@players = [Player.new, Player.new]
-    board = Board.new
-    board.display_board
     @@player_choices[@@players[0].name] = "0"
     @@player_choices[@@players[1].name] = "0"
-
+    play_game
   end
 
   def play_game
-    player_name = @@players[@@player_turn].name
-    puts "#{player_name}, mark your mark"
-    choice = gets.chomp
-    mark = players[@@player_turn].mark
-    board.mark_the_board(choice, mark)
-    @@player_choices[player_name] = 
-    @@player_turn = @@player_turn == 0 ? 1 : 0
+    board = Board.new
+    board.display_board
+    begin
+      player_name = @@players[@@player_turn].name
+      puts "#{player_name}, mark your mark"
+      choice = gets.chomp
+      mark = @@players[@@player_turn].mark
+      board.mark_the_board(choice, mark)
+      @@player_choices[player_name] += choice
+      
+      win = @@player_choices.values.map{|pattern| WIN_PATTERNS.include?(pattern)}.include?(true)
+
+      @@player_turn = @@player_turn == 0 ? 1 : 0
+    end while !win
   end
 end
